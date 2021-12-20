defac8891d6495f47231b0a34fbff0e8b071b3f9
killdiotxx <96296232+killdiotxx@users.noreply.github.com>
2021-12-20 11:08:40 -0300

    ejercicio2.py
    This is my first excercise.

---
+++ excersice2.py
@@ -0,0 +1,3 @@
+# An user wants to know the amount of money to withdraw from a bank after a month, with an interest of 2%.
+capital=int(input("Ingrese monto de capital:"))
+print("Interés despúes de un mes:",capital*0.02,"guaranies")
